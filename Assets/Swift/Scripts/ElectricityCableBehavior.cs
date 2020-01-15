5d1b4ed32ca2599f0937b35e9a396053ac3d3618
Thomas Couderc <thomas.couderc@viacesi.fr>
2020-01-15 09:11:27 +0100

    Added electricity colors

---
+++ Assets/Swift/Scripts/ElectricityCableBehavior.cs
@@ -0,0 +1,39 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectricityCableBehavior : MonoBehaviour
+{
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "ElectricCable")
+        {
+            Debug.Log("ELECTRICITY ENTER GRABB");
+            gameObject.GetComponentInParent<PhotonView>().RPC("ChangeBordersColor", RpcTarget.AllViaServer, true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "ElectricCable")
+        {
+            Debug.Log("ELECTRICITY Exit GRABB");
+            gameObject.GetComponentInParent<PhotonView>().RPC("ChangeBordersColor", RpcTarget.AllViaServer, false);
+        }
+    }
+}
