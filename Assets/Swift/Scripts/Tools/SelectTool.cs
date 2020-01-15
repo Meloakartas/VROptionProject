60a5c536fc14d4551d66215685953c960856e3ec
Arthur <ahmed.sdlv@gmail.com>
2020-01-15 14:15:32 +0100

    CameraRigMenu

---
+++ Assets/Swift/Scripts/Tools/SelectTool.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class SelectTool : MonoBehaviour
+{    
+    private ToolManager toolManager;
+    private string currentTool;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.GetComponent<ToolManager>() != null)
+        {
+            currentTool = other.gameObject.GetComponent<ToolManager>().CurrentTool;
+
+            switch (currentTool)
+            {
+                case "Grab":
+                    Destroy(other.gameObject.GetComponent<GrabTool>());
+                    break;
+                case "TopView":
+                    Destroy(other.gameObject.GetComponent<TopviewTool>());
+                    break;
+                case "Snap":
+                    Destroy(other.gameObject.GetComponent<GrabTool>());
+                    break;
+                case "SaveConfig":
+                    Destroy(other.gameObject.GetComponent<GrabTool>());
+                    break;
+                case "LoadConfig":
+                    Destroy(other.gameObject.GetComponent<GrabTool>());
+                    break;
+            }
+
+            switch (gameObject.name)
+            {
+                case "Grab":
+                    other.gameObject.AddComponent<GrabTool>();
+                    other.gameObject.GetComponent<ToolManager>().CurrentTool = "Grab";
+                    break;
+                case "TopView":
+                    other.gameObject.AddComponent<TopviewTool>();
+                    other.gameObject.GetComponent<ToolManager>().CurrentTool = "TopView";
+                    break;
+                case "Snap":
+                    other.gameObject.AddComponent<GrabTool>();
+                    other.gameObject.GetComponent<ToolManager>().CurrentTool = "Snap";
+                    break;
+                case "SaveConfig":
+                    other.gameObject.AddComponent<GrabTool>();
+                    other.gameObject.GetComponent<ToolManager>().CurrentTool = "SaveConfig";
+                    break;
+                case "LoadConfig":
+                    other.gameObject.AddComponent<GrabTool>();
+                    other.gameObject.GetComponent<ToolManager>().CurrentTool = "LoadConfig";
+                    break;
+            }
+        }
+    }
+}
---
+++ Assets/Swift/Scripts/Tools/ToolManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class ToolManager : MonoBehaviour
+{
+    public string CurrentTool;
+    public GameObject Menu;
+    private bool isMenuActive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isMenuActive = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(SteamVR_Actions._default.TopView.GetStateUp(SteamVR_Input_Sources.LeftHand))
+        {
+            Menu.SetActive(!isMenuActive);
+            isMenuActive = !isMenuActive;
+        }
+    }
+}
