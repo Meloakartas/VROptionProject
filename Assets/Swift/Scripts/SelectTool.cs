6b883bbd834cf8546b05307af445b15b343cc4a5
Arthur <ahmed.sdlv@gmail.com>
2020-01-15 09:42:13 +0100

    2.1
    still need to place the menu in camerarig prefab
    and remove awake() from tools

---
+++ Assets/Swift/Scripts/SelectTool.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class SelectTool : MonoBehaviour
+{    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.GetComponent<SteamVR_Behaviour_Pose>() != null)
+        {
+            Destroy(other.gameObject.GetComponent<BothControllersManager>() ??
+            other.gameObject.GetComponent<BothControllersManager>() ??
+            other.gameObject.GetComponent<BothControllersManager>() ??
+            other.gameObject.GetComponent<BothControllersManager>() ??
+            other.gameObject.GetComponent<BothControllersManager>());
+
+            switch (gameObject.name)
+            {
+                case "Grab":
+                    other.gameObject.AddComponent<BothControllersManager>();
+                    break;
+                case "TopView":
+                    other.gameObject.AddComponent<BothControllersManager>();
+                    break;
+                case "Snap":
+                    other.gameObject.AddComponent<BothControllersManager>();
+                    break;
+                case "SaveConfig":
+                    other.gameObject.AddComponent<BothControllersManager>();
+                    break;
+                case "LoadConfig":
+                    other.gameObject.AddComponent<BothControllersManager>();
+                    break;
+            }
+        }
+    }
+}
